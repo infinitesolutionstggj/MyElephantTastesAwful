791f8bfd7986359d977c99bd807fa92018aa98a0
AdamEaton <adam.eaton5@gmail.com>
2014-01-25 18:32:50 -0500

    Camera Scaling
    World is now 1080 units tall regardless of window size

--- META/META/META/Engine/Camera.cs
+++ META/META/META/Engine/Camera.cs
@@ -12,37 +12,49 @@ namespace META.Engine
 	{
 		public static SpriteBatch SpriteBatch;
 		public static Vector2 Pan;
+		public static float Scale;
 
 		public static void Initialize(SpriteBatch spriteBatch, Vector2 pan = new Vector2())
 		{
 			SpriteBatch = spriteBatch;
 			Pan = pan;
+			Scale = spriteBatch.GraphicsDevice.Viewport.Height / 1080f;
 		}
 
 		public static void Update(GameTime gameTime)
 		{
 			if (WorldToScreenPoint(Player.Main.position).X >= SpriteBatch.GraphicsDevice.Viewport.Width * 0.75f)
-				Pan.X = Player.Main.position.X - SpriteBatch.GraphicsDevice.Viewport.Width * 0.75f;
+			{
+				Vector2 screenPos = WorldToScreenPoint(Player.Main.position);
+				screenPos.X -= SpriteBatch.GraphicsDevice.Viewport.Width * 0.75f;
+				Pan.X = ScreenToWorldPoint(screenPos).X;
+			}
+				//Pan.X = Player.Main.position.X - SpriteBatch.GraphicsDevice.Viewport.Width * 0.75f;
 			else if (WorldToScreenPoint(Player.Main.position).X <= SpriteBatch.GraphicsDevice.Viewport.Width * 0.25f)
-				Pan.X = Player.Main.position.X - SpriteBatch.GraphicsDevice.Viewport.Width * 0.25f;
+			{
+				Vector2 screenPos = WorldToScreenPoint(Player.Main.position);
+				screenPos.X -= SpriteBatch.GraphicsDevice.Viewport.Width * 0.25f;
+				Pan.X = ScreenToWorldPoint(screenPos).X;
+			}
+				//Pan.X = Player.Main.position.X - SpriteBatch.GraphicsDevice.Viewport.Width * 0.25f;
 
-			Pan.X = (int)MathHelper.Clamp(Pan.X, 0, 10000);
+			Pan.X = (int)MathHelper.Clamp(Pan.X, 0, 9600 - SpriteBatch.GraphicsDevice.Viewport.Width);
 		}
 
 		public static void Draw(Texture2D texture, Rectangle destinationRectangle, Color color)
 		{
 			Vector2 screenPos = WorldToScreenPoint(new Vector2(destinationRectangle.X, destinationRectangle.Y));
-			SpriteBatch.Draw(texture, new Rectangle((int)screenPos.X, (int)screenPos.Y, destinationRectangle.Width, destinationRectangle.Height), color);
+			SpriteBatch.Draw(texture, new Rectangle((int)screenPos.X, (int)screenPos.Y, (int)(destinationRectangle.Width * Scale), (int)(destinationRectangle.Height * Scale)), color);
 		}
 
 		public static Vector2 WorldToScreenPoint(Vector2 worldPoint)
 		{
-			return worldPoint - Pan;
+			return (worldPoint - Pan) * Scale;
 		}
 
 		public static Vector2 ScreenToWorldPoint(Vector2 screenPoint)
 		{
-			return screenPoint + Pan;
+			return screenPoint / Scale + Pan;
 		}
 	}
 }
